de5a78fbabc6f17b1eacbaf2027c093852ebb9a7
kimianoorbakhsh <45353778+kimianoorbakhsh@users.noreply.github.com>
2022-12-09 20:49:46 +0330

    implement test files

--- Session 5/DesignPattern/src/test/java/JapaneseGardenCreatorTest.java
+++ Session 5/DesignPattern/src/test/java/JapaneseGardenCreatorTest.java
@@ -2,8 +2,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class JapaneseGardenCreatorTest {
+class JapaneseFlowerTest {
 
+    @Test
+    void testType() {
+        JapaneseGardenCreator japaneseGardenCreator = new FactoryCreator().createJapaneseGardenCreator();
+        ;
+        assertTrue(japaneseGardenCreator.createFlower() instanceof JapaneseFlower);
+    }
 
+    @Test
+    void testLocation() {
+        JapaneseGardenCreator japaneseGardenCreator = new FactoryCreator().createJapaneseGardenCreator();
+        ;
+        assertSame("Japan", japaneseGardenCreator.createFlower().location);
+    }
 
 }
